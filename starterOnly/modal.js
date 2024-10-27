e7ac3b6e19046159d7af2462e958ac5f1a8305e3
Jean Motte <jijimotte@gmail.com>
2024-10-27 15:32:07 +0100

    Init + start closing modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,32 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// closeBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  // modalbg.classList.remove("closing");
+  // document.body.classList.add("no-scroll");
 }
+
+// function closeModal() {
+//   modalbg.classList.add("closing");
+//   setTimeout(() => {
+//     modalbg.style.display = "none";
+//     modalbg.classList.remove("closing");
+//     document.body.classList.remove("no-scroll");
+//   }, 800);
+// }
+
+// document.addEventListener("keydown", (e) => {
+//   if (e.key === "Escape" && modalbg.style.display === "block") {
+//     closeModal()
+//   }
+// })
 
 
